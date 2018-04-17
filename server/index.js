14b72d04b3a2264b9e31dab7bca920a891c051ca
Hays <hays_xephon@outlook.com>
2018-04-18 00:30:15 +0800

    fix static router

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import Router from 'koa-router'
 import initRouterHandler from './router-imp'
 import { initDB } from './model/database'
 import serve from 'koa-static'
+import path from 'path'
 
 var app = new Koa()
 var router = new Router()
@@ -13,10 +14,11 @@ const logger = (ctx, next) => {
     console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`)
     next()
 }
-app.use(logger)
 
-console.log(__dirname)
-// app.use(serve(__dirname + '/router-imp'))
+// 这里logger不能放前面，否则会导致静态路由失效
+// TODO:这里需要把public文件区分开，例如webpack.config就不应该暴露出来
+app.use(serve(path.join(__dirname, '../..', 'client')))
+app.use(logger)
 initRouterHandler(router)
 app.use(router.routes())
 app.use(router.allowedMethods())
